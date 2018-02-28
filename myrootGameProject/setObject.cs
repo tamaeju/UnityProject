053e2f76321c216c24e41e92e0645f4dfd4af28e
tamaeju <taoyjk@gmail.com>
2018-02-28 23:43:59 +0900

    unity作業

---
+++ myrootGameProject/setObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class setObject
+{
+	Vector3 mypos;
+	public setObject(int x,int y)
+	{
+		mypos.x = x;
+		mypos.y = y;
+	}
+	public Vector3 returnPos() {
+		return mypos;
+	}
+
+}
