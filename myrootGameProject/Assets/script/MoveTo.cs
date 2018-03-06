2d6d39c2c23b410889eb51eef931a4d3ded93fba
MYCOMPUTER\appirits_1020520 <tamae@appirits.com>
2018-03-06 19:32:31 +0900

    unity作業

--- myrootGameProject/Assets/script/MoveTo.cs
+++ myrootGameProject/Assets/script/MoveTo.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class MoveTo : MonoBehaviour {
 
 	public Transform goal;
-
+	UnityEngine.AI.NavMeshAgent agent;
 	void Start() {
-		UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
 		agent.destination = goal.position;
 	}
 	public Vector3 returnMypos() {
 		return this.transform.position;
+	}
+	public void changeSpeed(float anAfterspeed) {
+		agent.speed = anAfterspeed;
 	} 
 }
