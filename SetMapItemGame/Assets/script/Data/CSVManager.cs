1f054a725d78b2533b041c256c04177790aa28e8
MYCOMPUTER\appirits_1020520 <tamae@appirits.com>
2018-05-17 15:47:30 +0900

    リソースフォルダからの読み込みへ修正

--- SetMapItemGame/Assets/script/Data/CSVManager.cs
+++ SetMapItemGame/Assets/script/Data/CSVManager.cs
@@ -1,133 +1,127 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.IO;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CSVManager : MonoBehaviour {//CSVデータの読み込みと書き込みを行うクラス
+public class CSVManager : MonoBehaviour { //CSVデータの読み込みと書き込みを行うクラス
 	StreamWriter m_sw;
-	int[][] stagedata;//何秒以内クリアか、必要捕食数のデータのデータ。（ゲームで実際に使用するのはstruct型の2次元配列）
+	int[][] stagedata; //何秒以内クリアか、必要捕食数のデータのデータ。（ゲームで実際に使用するのはstruct型の2次元配列）
 	[SerializeField]
 	Meditator meditator;
 
-	private int[,] getDataElement(string aDatapassANDname, int usingcolumnNum) {//データパスと使用するカラムを入力して使用する。
+	private int[, ] getDataElement (string aDatapassANDname, int usingcolumnNum) { //データパスと使用するカラムを入力して使用する。
 		int[][] dataElements;
-		int[,] practicalDataElements;
-		dataElements = getJagDataElement(aDatapassANDname);
-		practicalDataElements = parsePracticalDataElements(dataElements, usingcolumnNum);
+		int[, ] practicalDataElements;
+		dataElements = getJagDataElement (aDatapassANDname);
+		practicalDataElements = parsePracticalDataElements (dataElements, usingcolumnNum);
 		return practicalDataElements;
 	}
 
-	public int[,] getMapDataElement() {//データパスと使用するカラムを入力して使用する。
-		int usecolomnnum = Config.usecolomn_of_mapdata-1 ;
-		DataPathManager datapathmanager = meditator.getdatapathmanager();
-		string mapdatapass = datapathmanager.getmapdatapath();
-		return getDataElement(mapdatapass, usecolomnnum);
+	public int[, ] getMapDataElement () { //データパスと使用するカラムを入力して使用する。
+		int usecolomnnum = Config.usecolomn_of_mapdata - 1;
+		DataPathManager datapathmanager = meditator.getdatapathmanager ();
+		string mapdatapass = datapathmanager.getmapdatapath ();
+		return getDataElement (mapdatapass, usecolomnnum);
 	}
 
-
-
-	private int[][] getJagDataElement(string datapassANDname) {//ジャグデータをもらってから、それを2次元配列に入れる事が重要。その場合はint[][]からs
+	private int[][] getJagDataElement (string datapassANDname) { //ジャグデータをもらってから、それを2次元配列に入れる事が重要。その場合はint[][]からs
 		int[][] dataElements;
-		string textFile = datapassANDname;
-		System.Text.Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
-		string[] lines = System.IO.File.ReadAllLines(textFile, enc);
-		string[] RowStrings = lines[0].Split(',');
-
-
-		dataElements = new int[lines.Length][];
-		for (int i = 0; i < lines.Length; i++) {
-			dataElements[i] = new int[RowStrings.Length];
+		TextAsset bindata = Resources.Load (datapassANDname) as TextAsset; //指定したパスからテキストアセット型のデータを取得
+		Debug.LogFormat ("bindata, datapassANDnameは{0}、{1}", bindata, datapassANDname); //デバッグ用
+		string stringBindata = bindata.text; //
+		string[] lines = stringBindata.Split ('\n'); //\nで1行毎のデータに変換する。
+		Debug.LogFormat ("lines.Lengthは、{0}", lines.Length);
+		string[] RowStrings = lines[0].Split (','); //要素数を出すために、lines[0]を,で配列にわけて格納する。(このrowstrings自体は使用しない)
+		dataElements = new int[lines.Length - 1][]; //linesの要素数分データエレメントを作成
+		for (int i = 0; i < dataElements.Length; i++) {
+			dataElements[i] = new int[RowStrings.Length]; //RowStringsの要素数分データエレメントを作成
 		}
 
 		for (int j = 0; j < dataElements.Length; ++j) {
-			RowStrings = lines[j].Split(',');
-			for (int i = 0; i < dataElements[0].Length; ++i) {
-
-				dataElements[j][i] = Int32.Parse(RowStrings[i]);
+			RowStrings = lines[j].Split (','); //j番目のrowstringsを作成
+			for (int i = 0; i < dataElements[i].Length; ++i) {
+				dataElements[j][i] = Int32.Parse (RowStrings[i]);
 			}
 		}
 		return dataElements;
 	}
-	public int[][] getCCDataElement_needtoprocess() {
-		DataPathManager datapathmanager = meditator.getdatapathmanager();
-		return getJagDataElement(datapathmanager.getconditiondatapath());
+
+	public int[][] getCCDataElement_needtoprocess () {
+		DataPathManager datapathmanager = meditator.getdatapathmanager ();
+		return getJagDataElement (datapathmanager.getconditiondatapath ());
 	}
 
-	public int[][] getitemDataElement_needtoprocess() {
-		DataPathManager datapathmanager = meditator.getdatapathmanager();
-		return getJagDataElement(datapathmanager.getitemdatapath());
+	public int[][] getitemDataElement_needtoprocess () {
+		DataPathManager datapathmanager = meditator.getdatapathmanager ();
+		return getJagDataElement (datapathmanager.getitemdatapath ());
 	}
 
-	private int[,] parsePracticalDataElements(int[][] oldData, int usingcolumnNum) {//ジャグ配列からグリッド座標毎に1要素となるアイテムに対応した2次元配列への変換メソッド
-		int[,] practicalDataElements = new int[Config.maxGridNum, Config.maxGridNum];
-		for (int j = 0; j < practicalDataElements.GetLength(1); j++) {
-			for (int i = 0; i < practicalDataElements.GetLength(0); i++) {
-				practicalDataElements[i, j] = oldData[practicalDataElements.GetLength(0) * j + i][usingcolumnNum];
+	private int[, ] parsePracticalDataElements (int[][] oldData, int usingcolumnNum) { //ジャグ配列からグリッド座標毎に1要素となるアイテムに対応した2次元配列への変換メソッド
+		int[, ] practicalDataElements = new int[Config.maxGridNum, Config.maxGridNum];
+		for (int j = 0; j < practicalDataElements.GetLength (1); j++) {
+			for (int i = 0; i < practicalDataElements.GetLength (0); i++) {
+				practicalDataElements[i, j] = oldData[practicalDataElements.GetLength (0) * j + i][usingcolumnNum];
 			}
 		}
 		return practicalDataElements;
 	}
 
-
-	private void CSVSave<T>(string aDatapath, T writtendata, Action<T> act) {//アセットフォルダにtest.csvというファイルを作成する。
-		File.Delete(aDatapath);
+	private void CSVSave<T> (string aDatapath, T writtendata, Action<T> act) { //アセットフォルダにtest.csvというファイルを作成する。
+		File.Delete (aDatapath);
 		FileInfo fi;
-		fi = new FileInfo(aDatapath);
-		m_sw = fi.AppendText();
-		act(writtendata);
-		m_sw.Flush();
-		m_sw.Close();
-		Debug.Log("file was written");
+		fi = new FileInfo (aDatapath);
+		m_sw = fi.AppendText ();
+		act (writtendata);
+		m_sw.Flush ();
+		m_sw.Close ();
+		Debug.Log ("file was written");
 	}
 
-	public void MapCsvSave(int[,] writtendata) {//CSVSaveのジェネリック使用対応メソッド
-		DataPathManager datapathmanager = meditator.getdatapathmanager();
-		Action<int[,]> actaug = writeData;
-		CSVSave(datapathmanager.getmapdatapath(), writtendata, actaug);
+	public void MapCsvSave (int[, ] writtendata) { //CSVSaveのジェネリック使用対応メソッド
+		DataPathManager datapathmanager = meditator.getdatapathmanager ();
+		Action<int[, ]> actaug = writeData;
+		CSVSave (datapathmanager.getmapdatapath (), writtendata, actaug);
 	}
 
-	public void itemCsvSave(dragitemdata[,] writtendata) {//CSVSaveのジェネリック使用対応メソッド
-		DataPathManager datapathmanager = meditator.getdatapathmanager();
-		Action<dragitemdata[,]> actaug = writeData;
-		CSVSave(datapathmanager.getitemdatapath(), writtendata, actaug);
+	public void itemCsvSave (dragitemdata[, ] writtendata) { //CSVSaveのジェネリック使用対応メソッド
+		DataPathManager datapathmanager = meditator.getdatapathmanager ();
+		Action<dragitemdata[, ]> actaug = writeData;
+		CSVSave (datapathmanager.getitemdatapath (), writtendata, actaug);
 	}
 
-	public void cleardataCsvSave(clearconditiondata[] writtendata) {//CSVSaveのジェネリック使用対応メソッド
-		DataPathManager datapathmanager = meditator.getdatapathmanager();
+	public void cleardataCsvSave (clearconditiondata[] writtendata) { //CSVSaveのジェネリック使用対応メソッド
+		DataPathManager datapathmanager = meditator.getdatapathmanager ();
 		Action<clearconditiondata[]> actaug = writeData;
-		CSVSave(datapathmanager.getconditiondatapath(), writtendata, actaug);
+		CSVSave (datapathmanager.getconditiondatapath (), writtendata, actaug);
 	}
 
-	private void writeData(int[,] writtenData) {//オーバーライドメソッド
-		for (int j = 0; j < writtenData.GetLength(1); j++) {
-			for (int i = 0; i < writtenData.GetLength(0); i++) {
-				m_sw.WriteLine("{0},{1},{2}", i.ToString(), j.ToString(), writtenData[i, j].ToString());
+	private void writeData (int[, ] writtenData) { //オーバーライドメソッド
+		for (int j = 0; j < writtenData.GetLength (1); j++) {
+			for (int i = 0; i < writtenData.GetLength (0); i++) {
+				m_sw.WriteLine ("{0},{1},{2}", i.ToString (), j.ToString (), writtenData[i, j].ToString ());
 			}
 		}
-		Debug.Log("MapData was written");
+		Debug.Log ("MapData was written");
 	}
 
-	private void writeData(dragitemdata[,] writtenData) {//オーバーライドメソッド
-		for (int j = 0; j < writtenData.GetLength(0); j++) {
-			for (int i = 0; i < writtenData.GetLength(1); i++) {
-				m_sw.WriteLine("{0},{1},{2},{3}", j, i, writtenData[j, i].itemkind, writtenData[j, i].itemcount);
+	private void writeData (dragitemdata[, ] writtenData) { //オーバーライドメソッド
+		for (int j = 0; j < writtenData.GetLength (0); j++) {
+			for (int i = 0; i < writtenData.GetLength (1); i++) {
+				m_sw.WriteLine ("{0},{1},{2},{3}", j, i, writtenData[j, i].itemkind, writtenData[j, i].itemcount);
 			}
 		}
-		Debug.Log("itemdata was written");
+		Debug.Log ("itemdata was written");
 	}
 
-	private void writeData(clearconditiondata[] writtenData) {//オーバーライドメソッド
+	private void writeData (clearconditiondata[] writtenData) { //オーバーライドメソッド
 		for (int i = 0; i < writtenData.Length; i++) {
-			m_sw.WriteLine("{0},{1},{2}", i, writtenData[i].timelimit, writtenData[i].RequiredDeffenceCount);
+			m_sw.WriteLine ("{0},{1},{2}", i, writtenData[i].timelimit, writtenData[i].RequiredDeffenceCount);
 		}
-		Debug.Log("conditiondata was written");
+		Debug.Log ("conditiondata was written");
 	}
 
 }
-
-
-
--- SetMapItemGame/Assets/script/Data/DataPathManager.cs
+++ SetMapItemGame/Assets/script/Data/DataPathManager.cs
@@ -1,49 +1,48 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.IO;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DataPathManager : MonoBehaviour {//ゲームデータの保存パスを管理するクラス。csvmanagerとの連携度高
+public class DataPathManager : MonoBehaviour { //ゲームデータの保存パスを管理するクラス。csvmanagerとの連携度高
 
-	private string[] filename;//0はマップデータ、1はditemdata,2はclearcondinaldata
+	private string[] filename; //0はマップデータ、1はditemdata,2はclearcondinaldata
 	private string[] csvdatapath;
 	private string[] originfilename;
 	private int filekindlength = Config.filekindlength;
 
-
-	void Start() {
+	void Start () {
 		filename = new string[filekindlength];
 		csvdatapath = new string[filekindlength];
 		originfilename = new string[filekindlength];
 
 		originfilename[0] = "mapData";
-		originfilename[1] = "ditemData";
-		originfilename[2] = "clearconditionalData";
+		originfilename[1] = "ditemData0";
+		originfilename[2] = "clearconditionalData0";
 
 		for (int i = 0; i < filekindlength; i++) {
-			filename[i] = originfilename[i] + "0.csv";
-			csvdatapath[i] = Application.dataPath + "/data/" + filename[i];
+			filename[i] = originfilename[i];
+			csvdatapath[i] = "data/" + filename[i];
 		}
 	}
 
-	public string getmapdatapath() {
+	public string getmapdatapath () {
 		return csvdatapath[0];
 	}
-	public string getitemdatapath() {
+	public string getitemdatapath () {
 		return csvdatapath[1];
 	}
-	public string getconditiondatapath() {
+	public string getconditiondatapath () {
 		return csvdatapath[2];
 	}
 
-	public void ChangeMapCSVNum(int stagevalue) {//保存先かつ呼び出し元のファイルパスを変更する。
-		filename[0] = originfilename[0] + stagevalue.ToString() + ".csv";
-		csvdatapath[0] = Application.dataPath + "/data/" + filename[0];
-		Debug.Log(String.Format("{0}file was changed ", csvdatapath[0]));
+	public void ChangeMapCSVNum (int stagevalue) { //保存先かつ呼び出し元のファイルパスを変更する。
+		filename[0] = originfilename[0] + stagevalue.ToString ();
+		csvdatapath[0] = "data/" + filename[0];
+		Debug.Log (String.Format ("{0}file was changed ", csvdatapath[0]));
 	}
 	public enum datakind {
 		map,
